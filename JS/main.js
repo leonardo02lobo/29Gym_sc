2b6a7abf07d2e23df63ddb200f80b481eb74e0b6
FranklyBautista <franklybautista0511@gmail.com>
2025-01-05 18:48:20 -0800

    Se podria decir que esta listo

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,45 @@
-let botonwasa = []
+let botonwasa = [];
+const divs = [document.getElementById('div1'), document.getElementById('div2'), document.getElementById('div3'), document.getElementById('div4')];
 
 function iniciar() {
-    wasa()
+    wasa();
+    agregarEventosBotonesInfo();
 }
 
 function wasa() {
-    botonwasa = document.querySelectorAll('.BWasa') /*Solo funciona en class no en id*/
+    botonwasa = document.querySelectorAll('.BWasa'); /*Solo funciona en class no en id*/
 
-    botonwasa.forEach(boton=>{
+    botonwasa.forEach(boton => {
         boton.addEventListener('click', function () {
             window.open("https://wa.me/584247842240");
         });
-    })
-  
+    });
 }
 
-window.addEventListener('load', iniciar)
+function mostrarDiv(divId) {
+    divs.forEach(div => {
+        if (div.id === divId) {
+            if (div.style.display === 'block') {
+                div.style.display = 'none';
+                div.classList.remove('mostrar');
+            } else {
+                div.style.display = 'block';
+                setTimeout(() => div.classList.add('mostrar'), 10); // Añade un pequeño retraso para permitir que la transición ocurra
+            }
+        } else {
+            div.style.display = 'none';
+            div.classList.remove('mostrar');
+        }
+    });
+}
+
+function agregarEventosBotonesInfo() {
+    const botonesInfo = document.querySelectorAll('.info');
+    botonesInfo.forEach((boton, index) => {
+        boton.addEventListener('click', function () {
+            mostrarDiv(divs[index].id);
+        });
+    });
+}
+
+window.addEventListener('load', iniciar);
